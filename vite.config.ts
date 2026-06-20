8375787001a81741f522736cfe82c682c44a5097
saif799 <faycalbakziz@gmail.com>
2026-06-20 20:58:47 +0100

    nitro for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import { devtools } from '@tanstack/devtools-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
+import { nitro } from 'nitro/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
   plugins: [
+    nitro(),
     devtools(),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
